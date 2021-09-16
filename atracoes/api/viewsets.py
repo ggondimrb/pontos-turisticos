54094282bb6a5a688ef56e2ab8d40cbd32e6259d
Gabriel Gondim <ggondimrb@gmail.com>
2021-09-15 21:15:27 -0300

    first commit

---
+++ atracoes/api/viewsets.py
@@ -0,0 +1,11 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+from .serializers import AtracaoSerializer
+from atracoes.models import Atracao
+
+
+class AtracaoViewSet(ModelViewSet):
+    queryset = Atracao.objects.all()
+    serializer_class = AtracaoSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('nome', 'descricao')
---
+++ enderecos/api/serializers.py
@@ -0,0 +1,9 @@
+from enderecos.models import Endereco
+from rest_framework.serializers import ModelSerializer
+
+
+class EnderecoSerializer(ModelSerializer):
+    class Meta:
+        model = Endereco
+        fields = ['linha1', 'linha2', 'cidade',
+                  'estado', 'pais', 'latitude', 'longitude']
